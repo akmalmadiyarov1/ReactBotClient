4deb5b87b17b93dc1ba0526e1f3d754d05ca1e83
akmalmadiyarov1 <122540934+akmalmadiyarov1@users.noreply.github.com>
2023-10-30 16:41:54 +0500

    start

--- src/App.jsx
+++ src/App.jsx
@@ -77,7 +77,7 @@ const App = () => {
 
 	return (
 		<>
-			<h1 className='heading'>Sammi kurslar</h1>
+			<h1 className='heading'>Madiyarov kurslar</h1>
 			<Cart cartItems={cartItems} onCheckout={onCheckout} />
 			<div className='cards__container'>
 				{courses.map(course => (
