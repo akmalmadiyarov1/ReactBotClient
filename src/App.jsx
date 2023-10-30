8f4e33545f44c67a89d1458aef27ec865f3b70eb
akmalmadiyarov1 <122540934+akmalmadiyarov1@users.noreply.github.com>
2023-10-30 14:44:10 +0500

    send data

--- src/App.jsx
+++ src/App.jsx
@@ -52,33 +52,15 @@ const App = () => {
 		telegram.MainButton.show();
 	};
 
-	const onSendData = useCallback(() => {
-		const queryID = telegram.initDataUnsafe?.query_id;
+				const onSendData = useCallback(() => {
+					telegram.sendData(JSON.stringify(cartItems));
+				}, [cartItems]);
 
-		if (queryID) {
-			fetch(
-				'https://telegramwebapibot-b671371abfbb.herokuapp.com/web-data',
-				{
-					method: 'POST',
-					headers: {
-						'Content-Type': 'application/json',
-					},
-					body: JSON.stringify({
-						products: cartItems,
-						queryID: queryID,
-					}),
-				}
-			);
-		} else {
-			telegram.sendData(JSON.stringify(cartItems));
-		}
-	}, [cartItems]);
+				useEffect(()=>{
+					telegram.onEvent('mainButtonClicked', onSendData);
 
-	useEffect(() => {
-		telegram.onEvent('mainButtonClicked', onSendData);
-
-		return () => telegram.offEvent('mainButtonClicked', onSendData);
-	}, [onSendData]);
+					return()=>telegram.offEvent('mainButtonClicked', onSendData)
+				},[onSendData])
 
 	return (
 		<>
