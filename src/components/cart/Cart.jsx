2467b50e0691bb10875e6f2272136d8d392dc86c
akmalmadiyarov1 <122540934+akmalmadiyarov1@users.noreply.github.com>
2023-10-30 13:58:32 +0500

    return

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -2,22 +2,24 @@ import Button from '../button/button';
 import { totalPrice } from '../units/total-price';
 import './cart.css';
 
-const Cart = ({ cartItems, onCheckout }) => (
-  <div className='cart__container'>
-    <p>
-      Umumiy narx:{" "}
-      {totalPrice(cartItems).toLocaleString("en-US", {
-        style: "currency",
-        currency: "USD",
-      })}
-    </p>
-
-    <Button
-      title={`${cartItems.length === 0 ? 'Buyurtma berish' : 'Tolov'}`}
-      disable={cartItems.length === 0 ? true : false}
-      type={'checkout'}
-        onClick={onCheckout}/>
-  </div>
-);
+const Cart = ({ cartItems, onCheckout }) => {
+    return(
+      <div className='cart__container'>
+      <p>
+        Umumiy narx:{" "}
+        {totalPrice(cartItems).toLocaleString("en-US", {
+          style: "currency",
+          currency: "USD",
+        })}
+      </p>
+  
+      <Button
+        title={`${cartItems.length === 0 ? 'Buyurtma berish' : 'Tolov'}`}
+        disable={cartItems.length === 0 ? true : false}
+        type={'checkout'}
+          onClick={onCheckout}/>
+    </div>
+    )
+      };
 
 export default Cart;
